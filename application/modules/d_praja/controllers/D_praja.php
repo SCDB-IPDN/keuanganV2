9fa9c188b5e282489fa8712654310e19889d8f9f
Dicky Setiono <dsetiono1@gmail.com>
2020-11-17 09:50:00 +0700

    Update D_praja.php

--- application/modules/d_praja/controllers/D_praja.php
+++ application/modules/d_praja/controllers/D_praja.php
@@ -32,7 +32,31 @@ class D_praja extends CI_Controller{
     $this->load->view("include/panel");
     $this->load->view("include/footer");
   }
+	
+	public function edit_praja()
+     {
+          $input_data['id_praja'] = $this->input->post('id_praja', true);
+          $input_data['nama'] = $this->input->post('nama', true);
+          $input_data['tmpt_lahir'] = $this->input->post('tmpt_lahir', true);
+          $input_data['alamat'] = $this->input->post('alamat', true);
+          $input_data['tlp_pribadi'] = $this->input->post('tanggal_lahir', true);
+          $input_data['email'] = $this->input->post('email', true);
 
-  
+          $result = $this->D_praja_model->edit_praja($input_data);
 
+          if (!$result) { 							
+               $this->session->set_flashdata('praja', 'DATA PRAJA GAGAL DIUBAH.');		
+               redirect('d_praja/praja'); 			
+          } else { 								
+               $this->session->set_flashdata('praja', 'DATA PRAJA BERHASIL DIUBAH.');			
+               redirect('d_praja/praja'); 			
+          }
+     }
+
+     function hapus_praja()
+     {
+          $id_praja = $this->input->post('id_praja');
+          $this->D_praja_model->hapus_praja($id_praja);
+          redirect('d_praja/praja');
+     }
 }
