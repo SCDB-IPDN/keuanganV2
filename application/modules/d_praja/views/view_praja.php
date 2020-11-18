196b46d176f4b8fa2b0a6cfae872e9fdf99d502b
hasnaasd <48850541+hasnaasd@users.noreply.github.com>
2020-11-18 09:36:02 +0700

    editpraja

--- application/modules/d_praja/views/view_praja.php
+++ application/modules/d_praja/views/view_praja.php
@@ -30,7 +30,7 @@
           </button>
           <p>Silahkan input <b>Data Pegawai</b> Pada Button icon "<i class="fa fa-plus-square"></i>"</p>
         </div> -->
-       <div class ="table-responsive">
+        <div class ="table-responsive">
           <div class="panel-body">
             <table id="data-table-buttons" class="table table-striped table-bordered table-td-valign-middle">
               <thead>
@@ -39,7 +39,7 @@
                   <th>NAMA</th>
                   <TH>JENIS KELAMIN</TH>
                   <th>TINGKAT</th>
-		  <th>ASAL</th>
+                  <th>ASAL</th>
                   <th>OPSI</th>
                 </tr>
               </thead>
@@ -50,14 +50,14 @@
                    <td><?php echo $no++; ?></td>
                    <td><?= $x['nama']; ?></td>
                    <td><?= $x['jk']; ?></td>
-		   <td><?= $x['kab/kota']; ?></td>
                    <td><?= $x['tahun_masuk_kuliah'] - date('Y') +1 ;?></td>
+                   <td><?= $x['kab/kota']; ?></td>
                    <?php if (isset($x['nik_praja'])){ ?>
                     <td>
-                  <a href='<?= base_url().'d_praja/detail/'.$x['nik_praja'] ?>' class='btn btn-sm btn-primary' btn-sm><i class='fa fa-eye'></i></a>
-                  <a href="#" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editpraja<?php echo $x['id_praja'];?>"><i class="fa fas fa-edit"></i></a>
-                  <a href="#" class="btn btn-sm btn-danger" style="color:#fff;cursor:pointer" data-toggle="modal" data-target="#hapuspraja<?php echo $x['id_praja'];?>"><i class="fa fas fa-trash"></i></a>
-                  </td>
+                      <a href='<?= base_url().'d_praja/detail/'.$x['nik_praja'] ?>' class='btn btn-sm btn-primary' btn-sm><i class='fa fa-eye'></i></a>
+                      <a href="#" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editpraja<?php echo $x['nik_praja'];?>"><i class="fa fas fa-edit"></i></a>
+                      <a href="#" class="btn btn-sm btn-danger" style="color:#fff;cursor:pointer" data-toggle="modal" data-target="#hapuspraja<?php echo $x['nik_praja'];?>"><i class="fa fas fa-trash"></i></a>
+                    </td>
                   <?php } else { ?>
                     <td>Tidak ada detail</td>
                   <?php } ?>
@@ -72,9 +72,9 @@
     <!-- end panel -->
   </div>
   <!-- end col-10 -->
-		 <?php foreach(json_decode($data, true) as $x){?>
-    <!-- Modal EDIT PRAJA -->
-    <div class="modal fade" id="editpraja<?php echo $x['id_praja'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <?php foreach(json_decode($data, true) as $x){?>
+    <!-- Modal EDIT THL -->
+    <div class="modal fade" id="editpraja<?php echo $x['nik_praja'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -84,8 +84,8 @@
                 </button>
                 </div>
                 <div class="modal-body">
-                    <form action="edit_praja" method="POST">
-                        <input type="hidden" class="form-control" id="id_praja" name="id_praja" value="<?php echo $x['id_praja'];?>">
+                    <form action="d_praja/edit_praja" method="post">
+                        <input type="hidden" class="form-control" id="nik_praja" name="nik_praja" value="<?php echo $x['nik_praja'];?>">
                         <div class="form-group">
                             <label for="nama" class="col-form-label">Nama:</label>
                             <input type="text" class="form-control" id="nama" name="nama" value="<?php echo $x['nama'];?>" placeholder="Nama Lengkap.." required>
@@ -117,35 +117,7 @@
     </div>
     <?php } ?>
 
-    <?php foreach(json_decode($data, true) as $x){?>
-    <!-- Modal HAPUS PRAJA -->
-    <div class="modal fade" id="hapuspraja<?php echo $x['id_praja'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">HAPUS DATA PRAJA <?php echo $x['nama'];?></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                </div>
-                <div class="modal-body">
-                    <form class="form-horizontal" method="post" action="hapus_praja">
-                        <div class="modal-body">
-                            <p>Anda yakin mau menghapus Data PRAJA? <b><?php echo $x['nama'];?> ? </b></p>
-                        </div>
-                        <div class="modal-footer">
-                            <input type="hidden" name="id_praja" value="<?php echo $x['id_praja'];?>">
-                            <button class="btn" data-dismiss="modal" aria-hidden="true">Tutup</button>
-                            <button class="btn btn-danger">Hapus</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php } ?>
-</div>
-</div>
+
 
 <script src="<?php echo base_url().'assets/js/jquery.min.js'?>"></script>
 <script src="<?php echo base_url().'assets/js/raphael-min.js'?>"></script>
