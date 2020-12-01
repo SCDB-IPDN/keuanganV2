c11525b326b27657e7ba2ff30e024eb2b3409437
Dicky Setiono <dsetiono1@gmail.com>
2020-12-01 14:54:26 +0700

    Update D_praja.php

--- application/modules/d_praja/controllers/D_praja.php
+++ application/modules/d_praja/controllers/D_praja.php
@@ -11,38 +11,6 @@ class D_praja extends CI_Controller
 
   function index()
   {
-   if($_SESSION['nip'])
-   {
-    $data = $this->D_praja_model->get_praja()->result();
-    // $tingkat = $data[0]->tingkat-1;
-    // echo $tingkat;
-    // exit();
-    $x['data'] = json_encode($data);
-    // $x['tingkat'] = $tingkat;
-
-    $prov = $this->D_praja_model->get_provinsi()->result();
-    $x['prov'] = json_encode($prov);
-
-    $status = $this->D_praja_model->get_status()->result();
-    $x['status'] = json_encode($status);
-
-    $this->load->view("include/head");
-    $this->load->view("include/top-header");
-    $this->load->view("view_praja",$x);
-    $this->load->view("include/sidebar");
-    $this->load->view("include/panel");
-    $this->load->view("include/footer");
-  }else{
-   redirect("user");
- }
-}
-
-function detail($id)
-{
-  if($_SESSION['nip'])
-  {
-    $data = $this->D_praja_model->get_detail($id)->result();
-    $x['data'] = json_encode($data);
     if ($this->session->userdata('nip') != NULL) {
       $data = $this->D_praja_model->get_praja()->result();
       // $tingkat = $data[0]->tingkat-1;
@@ -60,6 +28,23 @@ function detail($id)
       $this->load->view("include/head");
       $this->load->view("include/top-header");
       $this->load->view("view_praja", $x);
+      $this->load->view("include/sidebar");
+      $this->load->view("include/panel");
+      $this->load->view("include/footer");
+    } else {
+      redirect("user");
+    }
+  }
+
+  function detail($id)
+  {
+    if ($this->session->userdata('nip') != NULL) {
+      $data = $this->D_praja_model->get_detail($id)->result();
+      $x['data'] = json_encode($data);
+
+      $this->load->view("include/head");
+      $this->load->view("include/top-header");
+      $this->load->view("view_detail", $x);
       $this->load->view("include/sidebar");
       $this->load->view("include/panel");
       $this->load->view("include/footer");
@@ -282,72 +267,6 @@ function alumni()
     $this->load->view("include/sidebar");
     $this->load->view("include/panel");
     $this->load->view("include/footer");
-  }else{
-   redirect("user");
- }
-
-}
-
-function edit($id)
-{
-  if($_SESSION['nip'])
-  {
-    $data = $this->D_praja_model->edit_praja($id)->result();
-    $x['data'] = json_encode($data);
-
-    $this->load->view("include/head");
-    $this->load->view("include/top-header");
-    $this->load->view("view_edit",$x);
-    $this->load->view("include/sidebar");
-    $this->load->view("include/panel");
-    $this->load->view("include/footer");
-  }else{
-   redirect("user");
- }
-
-}
-
-function edit_praja()
-{
-  if($_SESSION['nip'])
-  {
-    $input_data['nik_praja'] = $this->input->post('nik_praja', true);
-    $input_data['nama'] = $this->input->post('nama', true);
-    $input_data['email'] = $this->input->post('email', true);
-    $input_data['alamat'] = $this->input->post('alamat', true);
-    $input_data['rt'] = $this->input->post('rt', true);
-    $input_data['rw'] = $this->input->post('rw', true);
-    $input_data['nama_dusun'] = $this->input->post('nama_dusun', true);
-    $input_data['kelurahan'] = $this->input->post('kelurahan', true);
-    $input_data['kecamatan'] = $this->input->post('kecamatan', true);
-    $input_data['kab/kota'] = $this->input->post('kab/kota', true);
-    $input_data['kode_pos'] = $this->input->post('kode_pos', true);
-    $input_data['tlp_pribadi'] = $this->input->post('tlp_pribadi', true);
-    $input_data['status'] = $this->input->post('status', true);
-
-    if ($input_data['status'] == "turuntingkat") {
-      $input_data['tingkat'] = $this->input->post('tingkat', true)-1;
-    }else{
-      $input_data['tingkat'] = $this->input->post('tingkat', true);
-    }
-    
-
-    $result = $this->D_praja_model->edit_praja($input_data);
-
-    if (!$result) { 							
-     $this->session->set_flashdata('praja', 'DATA PRAJA GAGAL DIUBAH.');		
-     redirect('d_praja'); 			
-   } else { 						
-     $this->session->set_flashdata('praja', 'DATA PRAJA BERHASIL DIUBAH.');			
-     redirect('d_praja'); 			
-   }
-
- }else{
-   redirect("user");
- }
-}
-
-}
   } else {
     redirect("user");
   }
