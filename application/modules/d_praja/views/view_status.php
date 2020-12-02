d40f33d3081b4f2d7854158be5b913d864e5757e
hasnaasd <48850541+hasnaasd@users.noreply.github.com>
2020-12-02 10:30:14 +0700

    edittt bisaaaaaaa,semoga ga ada revisi lagi:(

--- application/modules/d_praja/views/view_status.php
+++ application/modules/d_praja/views/view_status.php
@@ -2,7 +2,7 @@
 <div id="content" class="content">
   <ol class="breadcrumb float-xl-right">
     <li class="breadcrumb-item"><a href="<?php echo base_url('home'); ?>">Dashboard</a></li>
-    <li class="breadcrumb-item"><a href="<?php echo base_url('d_praja'); ?>">All Praja</a></li>
+    <li class="breadcrumb-item"><a href="<?php echo base_url('d_praja'); ?>">Edit Status Praja</a></li>
 
   </ol>
   <h1 class="page-header">PRAJA</h1>
@@ -12,16 +12,17 @@
       <div class="panel-body">
         <div class="table-responsive">
 
-         
-          <h4 class="text-center">PRAJA IPDN <?php echo date("Y") ?> </h4>
-          <div id="graph" class="height-sm width-xl"></div>
+
+          <h4 class="text-center">STATUS PRAJA IPDN </h4>
+          
         </div>
       </div>
       <!-- end panel -->
       <div class="panel panel-inverse">
         <div class="panel-heading">
           <h4 class="panel-title">
-             <span><a href="<?php echo base_url('d_praja/editstatus');?>" class="btn btn-sm btn-green">UBAH STATUS PRAJA</a></span>
+            <span><a href="<?php echo base_url('d_praja/ubahstatus');?>" class="btn btn-sm btn-warning">UBAH STATUS PRAJA</a></span>
+            <!-- <span><a href="" class="btn btn-sm btn-red" data-toggle="modal" data-target="#editstatus">UBAH STATUS PRAJA</a></span> -->
             <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-plus-square"></i></button> -->
             <!-- <a href="" class="btn btn-icon btn-sm btn-inverse" data-toggle="modal" data-target="#addpeg"><i class="fa fa-plus-square"></i></a> -->
           </h4>
@@ -47,12 +48,12 @@
               <thead>
                 <tr>
                   <th>No</th>
+                  <th> ID </th>
                   <th>NAMA</th>
-                  <TH>JENIS KELAMIN</TH>
                   <th>TINGKAT</th>
                   <th>ANGKATAN</th>
                   <th>STATUS</th>
-                  <th>OPSI</th>
+                    <TH>KETERANGAN</TH>
                 </tr>
               </thead>
               <tbody>
@@ -60,19 +61,13 @@
                 <?php foreach (json_decode($data, true) as $x) : ?>
                   <tr>
                     <td><?php echo $no++; ?></td>
+                    <td><?= $x['id']; ?></td>
                     <td><?= $x['nama']; ?></td>
-                    <td><?= $x['jk']; ?></td>
                     <td><?= $x['tingkat']; ?></td>
                     <td><?= $x['angkatan']; ?>
                     <td><?= $x['status']; ?></td>
-                    <?php if (isset($x['id'])) { ?>
-                      <td>
-                        <a href='<?= base_url() . 'd_praja/detail/' . $x['id'] ?>' class='btn btn-sm btn-primary' btn-sm><i class='fa fa-eye'></i></a>
-                        <a href='<?= base_url() . 'd_praja/edt/' . $x['id'] ?>' class="btn btn-sm btn-warning" btn-sm><i class="fa fas fa-edit"></i></a>
-                      </td>
-                    <?php } else { ?>
-                      <td>Tidak ada detail</td>
-                    <?php } ?>
+                    <td><?= $x['keterangan']; ?></td>
+                    
                   </tr>
                 <?php endforeach; ?>
               </tbody>
@@ -90,20 +85,3 @@
     <script src="<?php echo base_url() . 'assets/js/jquery.min.js' ?>"></script>
     <script src="<?php echo base_url() . 'assets/js/raphael-min.js' ?>"></script>
     <script src="<?php echo base_url() . 'assets/js/morris.min.js' ?>"></script>
-
-    <script>
-      Morris.Bar({
-        element: 'graph',
-        data: <?php echo $prov; ?>,
-        xkey: 'provinsi',
-        ykeys: ['jumlah'],
-        labels: [' Jumlah'],
-        barRatio: 0.1,
-        behaveLikeLine: true,
-        pointSize: 5,
-        resize: true,
-        parseTime: false,
-        hideHover: 'auto',
-        gridTextSize: 10
-      });
-    </script>
