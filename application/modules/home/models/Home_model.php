fe9f7193e497b8be957c89d29f01ef0b1f02edde
Maulyanda <maulyanda94.kcb@gmail.com>
2020-12-04 12:12:23 +0700

    Update home span jatinangor

--- application/modules/home/models/Home_model.php
+++ application/modules/home/models/Home_model.php
@@ -152,14 +152,14 @@ class Home_model extends CI_Model{
 	}
 
 	public function get_span_jatinangor(){
-		$result = $this->db->query("SELECT persentase_t FROM tbl_span WHERE kode_satker = 448302 ORDER BY created_date DESC LIMIT 1")->result();
+		$result = $this->db->query("SELECT CONCAT(FORMAT(100*(real_peg+real_mod+real_bar)/(pagu_peg+pagu_mod+pagu_bar), 2), '%') as persentase_t FROM tbl_spanint WHERE satker = 448302")->result(); 
 
 		return $result;
 	}
 
 	public function get_all_span_biro()
 	{	
-		$result = $this->db->query("SELECT *, alias FROM (SELECT a.*, b.alias FROM tbl_span_biro as a JOIN tbl_satker_biro as b ON a.kode_satker_biro=b.kode_satker_biro ORDER BY a.id_span_biro DESC limit 4) as k ORDER BY alias ASC")->result();
+		$result = $this->db->query("SELECT alias, CONCAT(FORMAT(100*(real_peg+real_mod+real_bar)/(pagu_peg+pagu_mod+pagu_bar), 2), '%') as persentase_t FROM tbl_spanint JOIN tbl_satker_biro ON satker = kode_satker_biro  WHERE LENGTH(satker) = 4 ORDER BY alias")->result();
 
 		return $result;
 	}
