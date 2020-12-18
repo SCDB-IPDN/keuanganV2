48399dbbc1cf0a666b87e3d1095b9b19bb64059f
hasnaasd <48850541+hasnaasd@users.noreply.github.com>
2020-12-18 16:18:29 +0700

    ngekomen yang upload

--- application/modules/d_praja/views/view_ubah.php
+++ application/modules/d_praja/views/view_ubah.php
@@ -56,8 +56,8 @@
 
                 <br>
 
-                  <label for="basic-url">Upload SK : </label>
-                  <input type="file" class="btn btn-light btn-lg-5" name="fileToUpload" id="fileToUpload">
+                 <!--  <label for="basic-url">Upload SK : </label>
+                  <input type="file" class="btn btn-light btn-lg-5" name="fileToUpload" id="fileToUpload"> -->
                   <br>
               </div>
             </div>
