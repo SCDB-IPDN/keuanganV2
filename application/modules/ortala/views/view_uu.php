55a5d386f327c2ceaf9b1e5b0b18f58552eaf049
restiyanada <rfziog@gmail.com>
2021-01-24 10:31:27 +0700

    contoh resolve conflict

--- application/modules/ortala/views/view_uu.php
+++ application/modules/ortala/views/view_uu.php
@@ -170,7 +170,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                	<h5 class="modal-title" id="delprokumm">Hapus Produk Hukum</h5>
+                	<h5 class="modal-title" id="delprokumm">Haaaaaaapus Produk Hukum</h5>
                 	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     	<span aria-hidden="true">&times;</span>
                 	</button>
