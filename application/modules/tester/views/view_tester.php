8a7b23c127630013e92c60f2f4af8b6318f383f2
restiyanada <rfziog@gmail.com>
2021-01-26 08:45:53 +0700

    tester

---
+++ application/modules/tester/views/view_tester.php
@@ -0,0 +1,28 @@
+<div id="content" class="content">
+  <ol class="breadcrumb float-xl-right">
+    <li class="breadcrumb-item"><a href="<?php echo base_url('home');?>">Dashboard</a></li>
+    <li class="breadcrumb-item"><a href="<?php echo base_url('tester');?>">Tester</a></li>
+  </ol>
+  <h1 class="page-header">Tester</h1>
+  <div class="row">
+    <div class="col-xl-12">
+      <div class="panel panel-inverse">
+        <div class="panel-heading">
+          <h4 class="panel-title">
+          </h4>
+          <div class="panel-heading-btn">
+            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-redo"></i></a>
+            <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+            <!-- <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a> -->
+          </div>
+        </div>
+        <div class="panel-body">
+        
+        </div>
+        <!-- end panel-body -->
+      </div>
+      <!-- end panel -->
+    </div>
+    <!-- end col-10 -->
+</div>
