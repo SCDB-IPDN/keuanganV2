ef7f2b6987476eff42d13cf5a6033ce982e4b674
restiyanada <rfziog@gmail.com>
2021-01-27 14:33:17 +0700

    fixing date

--- application/modules/home/views/home.php
+++ application/modules/home/views/home.php
@@ -725,7 +725,7 @@
 				<div class="panel-body bg-dark">
 					<div class="card border-0 bg-dark text-white text-truncate mb-3">
 						<div class="mb-3 text-grey">
-							<b class="mb-3">TOTAL PRODUK HUKUM <br>(<?php echo$last_updated; ?>)</b> 
+							<b class="mb-3">TOTAL PRODUK HUKUM</b> 
 							<span class="ml-2"><i class="fa fa-info-circle" data-toggle="popover" data-trigger="hover" data-title="Institut Pendidikan Dalam Negeri (IPDN)" data-placement="top" data-original-title="" title=""></i></span>
 							<div class="text-grey">
 								<i class=""></i> KAMPUS IPDN
