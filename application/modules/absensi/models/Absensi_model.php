98eeeb2ed42a1da67f282d590912774ac36fd61f
hasnaasd <48850541+hasnaasd@users.noreply.github.com>
2021-02-18 17:16:32 +0700

    Update Absensi_model.php

--- application/modules/absensi/models/Absensi_model.php
+++ application/modules/absensi/models/Absensi_model.php
@@ -75,7 +75,7 @@
        FROM(SELECT DISTINCT min(ab.waktu) as MASUK, max(ab.waktu) as KELUAR, us.nama,ab.id_absen,ab.tgl,ab.id_user, ab.penugasan
        FROM absensi ab JOIN tbl_users_presensi us ON ab.id_user=us.id_user WHERE ab.penugasan = $penugasan and  ab.tgl between '$fromDate' and '$endDate'
        GROUP BY ab.id_user,ab.tgl,ab.penugasan
-       ORDER BY ab.tgl DESC,ab.id_user) a");
+       ORDER BY ab.tgl DESC,ab.id_user) a ");
 
       return $result;
     }
