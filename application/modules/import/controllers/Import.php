e6ddc91496eaff596f9356d27c64e1b64da5b1eb
Maulyanda <maulyanda94.kcb@gmail.com>
2021-03-09 15:31:08 +0700

    tambahan import kelas kuliah

---
+++ application/modules/import/controllers/Import.php
@@ -0,0 +1,123 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed ');
+
+require('./application/third_party/phpoffice/vendor/autoload.php');
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+
+class Import extends CI_Controller 
+{
+
+    public function __construct(){
+        parent::__construct();
+        //load model
+        $this->load->model('import_model');
+    }
+
+    // Kelas Kuliah
+
+    public function view_kk(){
+        $x['prodi'] = $this->import_model->getprodi();
+
+        $this->load->view("include/head");
+        $this->load->view("include/top-header");
+        $this->load->view('kelas_kuliah',$x);
+        $this->load->view("include/sidebar");
+        $this->load->view("include/panel");
+        $this->load->view("include/footer");
+    }
+    
+    public function kelas_kuliah($data){
+        $x['kode_prodi'] = $data;
+
+        $this->load->view("include/head");
+        $this->load->view("include/top-header");
+        $this->load->view('kelas_kuliah', $x);
+        $this->load->view("include/sidebar");
+        $this->load->view("include/panel");
+        $this->load->view("include/footer");
+    }
+
+    public function datakk($data){
+        $data = $this->import_model->getkk($data);
+        $hasil = array();
+
+        foreach($data as $r){
+            $semester = $r->semester == NULL ? "<i><font>Tidak ada data</font></i>": $r->semester;
+            $kode_mk = $r->kode_mk == NULL ? "<i><font>Tidak ada data</font></i>": $r->kode_mk;
+            $nama_mk = $r->nama_mk == NULL ? "<i><font>Tidak ada data</font></i>": $r->nama_mk;
+            $kelas = $r->kelas == NULL ? "<i><font>Tidak ada data</font></i>": $r->kelas;
+            $semester = $r->semester == NULL ? "<i><font>Tidak ada data</font></i>": $r->semester;
+            $bahasan = $r->bahasan == NULL ? "<i><font>Tidak ada data</font></i>": $r->bahasan;
+            $tanggal_mulai = $r->tanggal_mulai_efektif == NULL ? "<i><font>Tidak ada data</font></i>": $r->tanggal_mulai_efektif;
+            $tanggal_akhir = $r->tanggal_akhir_efektif == NULL ? "<i><font>Tidak ada data</font></i>": $r->tanggal_akhir_efektif;
+
+            $hasil[] = array(
+                $semester,
+                $kode_mk,
+                $nama_mk,
+                $kelas,
+                $bahasan,
+                $tanggal_mulai,
+                $tanggal_akhir
+            );
+        }
+        
+        echo json_encode($hasil);
+    }
+
+    public function uploadkk()
+    {        
+        $kode_prodi = $this->input->post('kode_prodi');
+        
+        $file_mimes = array('application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        if(isset($_FILES['kk']['name']) && in_array($_FILES['kk']['type'], $file_mimes)) {
+
+            $arr_file = explode('.', $_FILES['kk']['name']);
+            $extension = end($arr_file);
+
+            if($extension != 'xlsx') {
+                $this->session->set_flashdata('kelas_kuliah', '<div class="alert alert-success"><b>PROSES IMPORT DATA GAGAL!</b> Format file yang anda masukkan salah!</div>');
+
+                redirect("import/kelas_kuliah/$kode_prodi"); 
+            } else {
+                $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+            }
+
+            $loadexcel  = $reader->load($_FILES['kk']['tmp_name']);
+            $list_sheet = $loadexcel->getSheetNames();
+            $sheetData = $loadexcel->getSheetByName($list_sheet[0])->toArray(null, true, true ,true);
+
+            $data = array();
+            $numrow = 0;
+
+            foreach($sheetData as $row){
+                if($numrow > 0){
+                    
+                    if($row['A'] != NULL){             
+                        array_push($data, array(
+                            'semester'                  => $row['A'],
+                            'kode_mk'                   => $row['B'],
+                            'nama_mk'                   => $row['C'],
+                            'kelas'                     => $row['D'],
+                            'bahasan'                   => $row['E'],
+                            'tanggal_mulai_efektif'     => $row['F'],
+                            'tanggal_akhir_efektif'     => $row['G'],
+                            'prodi'     => $kode_prodi 
+                        ));
+                    }
+                }
+               $numrow++;
+            }
+
+            if($this->import_model->kelas_kuliah('tbl_kelas_kuliah', $data)){
+                redirect('import/kelas_kuliah/'.$kode_prodi);
+            }else{
+                redirect('import/kelas_kuliah/'.$kode_prodi);
+            }
+        }
+    }
+
+}
---
+++ application/modules/import/views/kelas_kuliah.php
@@ -0,0 +1,112 @@
+<div id="content" class="content">
+    <ol class="breadcrumb float-xl-right">
+        <li class="breadcrumb-item"><a href="<?php echo base_url('import/kelas_kuliah');?>">Kelas Kuliah</a></li>
+    </ol>
+    <h1 class="page-header">Manage Kelas Kuliah</h1>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="panel panel-inverse">
+                <div class="panel-heading">
+                    <h4 class="panel-title"></h4>
+                    <div class="panel-heading-btn">
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-redo"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+                    </div>
+                </div>
+
+                <?php if($this->uri->segment(2) == "view_kk") { ?>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="panel-body">
+                                <table id="data-table-buttons" class="table table-striped table-bordered table-td-valign-middle">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-nowrap">No</th>
+                                            <th class="text-nowrap">Nama Prodi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php $no = 1; foreach($prodi as $r) { ?>
+                                        <tr>
+                                            <td><?= $no++ ?></td>
+                                            <td><a href="<?= base_url('import/kelas_kuliah/'.$r->kode_prodi); ?>"><?= $r->nama_program_studi; ?></a></td>
+                                        </tr>
+                                    <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+
+                <?php if($this->uri->segment(2) == "kelas_kuliah") { ?>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-xl-7 col-lg-8">
+                            <?php echo $this->session->flashdata('kelas_kuliah') ?>
+                            <form method="POST" action="<?php echo base_url() ?>import/uploadkk" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail2">Upload Data Excel</label>
+                                    <span class="ml-2">
+                                        <i class="fa fa-info-circle" data-toggle="popover" data-trigger="hover" data-title="Format yang diupload .xlsx" data-placement="top" data-content=""></i>
+                                    </span>
+                                    <input type="file" name="kk" class="form-control">
+                                    <input type="hidden" name="kode_prodi" value="<?= $kode_prodi ?>">
+                                </div>
+                                <button type="submit" class="btn btn-success">Upload Data</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="panel-body">
+                    <table id="data-kelas-kuliah" class="table table-striped table-bordered table-td-valign-middle">
+                        <thead>
+                            <tr>
+                                <th class="text-nowrap">Semester</th>
+                                <th class="text-nowrap">Kode Matakuliah</th>
+                                <th class="text-nowrap">Nama Matakuliah</th>
+                                <th class="text-nowrap">Nama Kelas</th>
+                                <th class="text-nowrap">Bahasan</th>
+                                <th class="text-nowrap">Tanggal Mulai Efektif</th>
+                                <th class="text-nowrap">Tanggal Akhir Efektif</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+                <?php } ?>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
+<script>
+$(document).ready(function() {
+
+    if ($('#data-kelas-kuliah').length !== 0) {
+        var url = "<?php echo base_url('import/datakk/'.$kode_prodi); ?>";
+        console.log(url);
+
+        $('#data-kelas-kuliah').dataTable({
+            // dom: 'Bfrtip',
+            dom: '<"row"<"col-sm-5"B><"col-sm-7"fr>>t<"row"<"col-sm-5"i><"col-sm-7"p>>',
+            buttons: [
+                'copy', 'excel', 'print'
+            ],
+            responsive: true,
+            "ajax": {
+                "url": url,
+                "dataSrc": ""
+            }
+        });
+    }
+});
+</script>
+
+       
