114568e2dbf29848d36a449b868a8a29486e0a98
hasnaasd <48850541+hasnaasd@users.noreply.github.com>
2021-03-12 16:47:25 +0700

    KURKUM MERGE dari DEV

--- application/modules/import/controllers/Import.php
+++ application/modules/import/controllers/Import.php
@@ -752,4 +752,327 @@ class Import extends CI_Controller
         redirect('import/akm/'.$kode_prodi);
     }
 
+           // kurikulum
+
+     public function view_kurkum(){
+        $x['prodi'] = $this->import_model->getprodi();
+        // var_dump($x['prodi']);exit();
+    
+        $this->load->view("include/head");
+        $this->load->view("include/top-header");
+        $this->load->view('kurikulum',$x);
+        $this->load->view("include/sidebar");
+        $this->load->view("include/panel");
+        $this->load->view("include/footer");
+    }
+
+    public function kurkum_pamong($data){
+        $x['kode_prodi'] = $data;
+        
+        $x['getkurkum'] = $this->import_model->getkurkum($data);
+
+        $ho = $this->import_model->getkurkum($data);
+        $x['ho'] = json_encode($ho);
+
+        
+        $this->load->view("include/head");
+        $this->load->view("include/top-header");
+        $this->load->view('kurikulum', $x);
+        $this->load->view("include/sidebar");
+        $this->load->view("include/panel");
+        $this->load->view("include/footer");
+    }
+
+
+
+    public function kurkum_matkul(){
+        $x['getkurkum'] = $this->uri->segment(4);
+        $x['getprodi'] = $this->uri->segment(3);
+
+        // var_dump($this->uri->segment(4));exit;
+        $this->load->view("include/head");
+        $this->load->view("include/top-header");
+        $this->load->view('kurikulum',$x);
+        $this->load->view("include/sidebar");
+        $this->load->view("include/panel");
+        $this->load->view("include/footer");
+    }
+
+    public function uploadkurkum()
+    {        
+        $nama_kurikulum = $this->input->post('getkurkum');
+        $prodi = $this->input->post('getprodi');
+        // var_dump($prodi);exit;
+        
+        $file_mimes = array('application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        if(isset($_FILES['kurkum']['name']) && in_array($_FILES['kurkum']['type'], $file_mimes)) {
+
+            $arr_file = explode('.', $_FILES['kurkum']['name']);
+            $extension = end($arr_file);
+
+            if($extension != 'xlsx') {
+                $this->session->set_flashdata('kurkum_matkul', '<div class="alert alert-success"><b>PROSES IMPORT DATA GAGAL!</b> Format file yang anda masukkan salah!</div>');
+
+                redirect("import/kurkum_matkul/'.$prodi.'/'.$nama_kurikulum"); 
+            } else {
+                $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+            }
+
+            $loadexcel  = $reader->load($_FILES['kurkum']['tmp_name']);
+            $list_sheet = $loadexcel->getSheetNames();
+            $sheetData = $loadexcel->getSheetByName($list_sheet[0])->toArray(null, true, true ,true);
+
+            $data = array();
+            $numrow = 0;
+
+            foreach($sheetData as $row){
+                if($numrow > 0){
+                    
+                    if($row['A'] != NULL){             
+                        array_push($data, array(
+                            'kode_mk'                  => $row['A'],
+                            'nama_mk'                  => $row['B'],
+                            'jenis_mk'                 => $row['C'],
+                            'sks_tatapmuka'            => $row['D'],
+                            'sks_praktek'              => $row['E'],
+                            'sks_lapangan'             => $row['F'],
+                            'sks_simulasi'             => $row['G'],
+                            'tgl_mulai_efektif'        => $row['H'],
+                            'tgl_akhir_efektif'        => $row['I'],
+                            'semester'                 => $row['J'],
+                            'nama_kurikulum'           => $nama_kurikulum,
+                            'prodi'                    => $prodi
+                        ));
+                    }
+                }
+               $numrow++;
+            }
+
+            if($this->import_model->kurkum_matkul('tbl_kurkum_matkul', $data)){
+                redirect('import/kurkum_matkul/'.$prodi.'/'.$nama_kurikulum);
+            }else{
+                redirect('import/kurkum_matkul/'.$prodi.'/'.$nama_kurikulum);
+            }
+        }
+    }
+
+    public function datakurkum($data){
+        $data = $this->import_model->getkurkum($data);
+        $hasil = array();
+
+        foreach($data as $r){
+
+
+            $nama_kurikulum ="<a href='../kurkum_matkul/$r->prodi/$r->nama_kurikulum'></i>$r->nama_kurikulum</a>";
+            $mulai_kurikulum = $r->mulai_kurikulum == NULL ? "<i><font>Tidak ada data</font></i>": $r->mulai_kurikulum;
+            $lulus = $r->lulus == NULL ? "<i><font>Tidak ada data</font></i>": $r->lulus;
+            $wajib = $r->wajib == NULL ? "<i><font>Tidak ada data</font></i>": $r->wajib;
+            $pilihan = $r->pilihan == NULL ? "<i><font>Tidak ada data</font></i>": $r->pilihan;
+
+            if($this->session->userdata('role') == 'Admin' || $this->session->userdata('role') == 'SuperAdmin' || $this->session->userdata('role') == 'Akademik'){
+                $opsi = "<a 
+                href='javascript:;' data-nama_kurikulum='$r->nama_kurikulum' data-mulai_kurikulum='$r->mulai_kurikulum' 
+                data-lulus='$r->lulus' data-wajib='$r->wajib' data-pilihan='$r->pilihan'
+                data-toggle='modal' data-target='#edit-data' class='btn btn-info'><i class='fa fas fa-edit'></i>
+                </a>
+
+                <a 
+                href='javascript:;' data-nama_kurikulum='$r->nama_kurikulum'
+                data-toggle='modal' data-target='#hapus-kurikulum' class='btn btn-danger'><i class='fa fas fa-trash'></i>
+                </a>";
+            }else{
+                $opsi = "";
+            }
+        
+            $hasil[] = array(
+                $nama_kurikulum,
+                $mulai_kurikulum,
+                $lulus,
+                $wajib,
+                $pilihan,
+                $opsi
+            );
+        }
+        
+        echo json_encode($hasil);
+    }
+
+
+    public function datamatkulkurum(){
+        $prodi = $this->uri->segment(3);
+        // var_dump($prodi);exit();
+        $nama_kurikulum = $this->uri->segment(4);
+
+        $data = $this->import_model->getmatkulkurkum($prodi,$nama_kurikulum);
+        // var_dump($data);exit;
+        $hasil = array();
+
+        foreach($data as $r){
+
+            
+            $kode_mk = $r->kode_mk == NULL ? "<i><font>Tidak ada data</font></i>": $r->kode_mk;
+            $nama_mk = $r->nama_mk == NULL ? "<i><font>Tidak ada data</font></i>": $r->nama_mk;
+            $jenis_mk = $r->jenis_mk == NULL ? "<i><font>Tidak ada data</font></i>": $r->jenis_mk;
+            $sks_tatapmuka = $r->sks_tatapmuka == NULL ? "<i><font>Tidak ada data</font></i>": $r->sks_tatapmuka;
+            $sks_praktek = $r->sks_praktek == NULL ? "<i><font>Tidak ada data</font></i>": $r->sks_praktek;
+            $sks_lapangan = $r->sks_lapangan == NULL ? "<i><font>Tidak ada data</font></i>": $r->sks_lapangan;
+            $sks_simulasi = $r->sks_simulasi == NULL ? "<i><font>Tidak ada data</font></i>": $r->sks_simulasi;
+            $tgl_mulai_efektif = $r->tgl_mulai_efektif == NULL ? "<i><font>Tidak ada data</font></i>": $r->tgl_mulai_efektif;
+            $tgl_akhir_efektif = $r->tgl_akhir_efektif == NULL ? "<i><font>Tidak ada data</font></i>": $r->tgl_akhir_efektif;
+            $semester = $r->semester == NULL ? "<i><font>Tidak ada data</font></i>": $r->semester;
+            // $nama_kurikulum = $r->nama_kurikulum == NULL ? "<i><font>Tidak ada data</font></i>": $r->nama_kurikulum;
+            
+
+            if($this->session->userdata('role') == 'Admin' || $this->session->userdata('role') == 'SuperAdmin' || $this->session->userdata('role') == 'Akademik'){
+                $opsi = "<a 
+                href='javascript:;' data-kode_mk='$r->kode_mk' data-nama_mk='$r->nama_mk' 
+                data-jenis_mk='$r->jenis_mk' data-sks_tatapmuka='$r->sks_tatapmuka' data-sks_praktek='$r->sks_praktek'
+                data-sks_praktek='$r->sks_lapangan' data-sks_praktek='$r->sks_simulasi' data-sks_praktek='$r->tgl_mulai_efektif' 
+                data-tgl_akhir_efektif='$r->tgl_akhir_efektif' data-semester='$r->semester'
+                data-toggle='modal' data-target='#edit-data-matkul' class='btn btn-info'><i class='fa fas fa-edit'></i>
+                </a>
+
+                <a 
+                href='javascript:;' data-kode_mk='$r->kode_mk'
+                data-toggle='modal' data-target='#hapus-kurikulum-matkul' class='btn btn-danger'><i class='fa fas fa-trash'></i>
+                </a>";
+            }else{
+                $opsi = "";
+            }
+        
+            $hasil[] = array(
+                $kode_mk ,
+                $nama_mk ,
+                $jenis_mk ,
+                $sks_tatapmuka ,
+                $sks_praktek,
+                $sks_lapangan,
+                $sks_simulasi ,
+                $tgl_mulai_efektif,
+                $tgl_akhir_efektif,
+                $semester,
+                // $nama_kurikulum,
+                $opsi
+            );
+        }
+        
+        echo json_encode($hasil);
+    }
+
+    function tambahkurikulum(){
+
+        $input_data['nama_kurikulum'] = $this->input->post('nama_kurikulum', true);
+        $input_data['mulai_kurikulum'] = $this->input->post('mulai_kurikulum', true);
+        $input_data['lulus'] = $this->input->post('lulus', true);
+        $input_data['wajib'] = $this->input->post('wajib', true);
+        $input_data['pilihan'] = $this->input->post('pilihan', true);
+        $input_data['prodi'] = $this->input->post('prodi', true); 
+
+        $prod =  $this->input->post('prodi', true); 
+
+        $result = $this->import_model->tambahkurikulum($input_data);
+        // var_dump($result);exit;
+
+        if (!$result) {                             
+            $this->session->set_flashdata('kurkum', 'DATA KURIKULUM GAGAL DITAMBAHKAN!!');      
+            redirect('import/kurkum_pamong/'.$prod);            
+        } else {                                
+            $this->session->set_flashdata('kurkum', 'DATA KURIKULUM BERHASIL DITAMBAHKAN.');            
+            redirect('import/kurkum_pamong/'.$prod);            
+        }
+    }
+
+    function ubahkurikulum(){
+
+    
+        $ubahkurkum['nama_kurikulum'] = $this->input->post('nama_kurikulum', true);
+        $ubahkurkum['mulai_kurikulum'] = $this->input->post('mulai_kurikulum', true);
+        $ubahkurkum['lulus'] = $this->input->post('lulus', true);
+        $ubahkurkum['wajib'] = $this->input->post('wajib', true);
+        $ubahkurkum['pilihan'] = $this->input->post('pilihan', true);
+        $ubahkurkum['prodi'] = $this->input->post('prodi', true);
+        
+        $prod =  $this->input->post('prodi', true); 
+
+        $result = $this->import_model->ubah_kurikulum($ubahkurkum);
+
+        if (!$result) {                             
+            $this->session->set_flashdata('kurkum', 'DATA KURIKULUM GAGAL DIUBAH.');        
+            redirect('import/kurkum_pamong/'.$prod);            
+        } else {                                
+            $this->session->set_flashdata('kurkum', 'DATA KURIKULUM BERHASIL DIUBAH.');         
+            redirect('import/kurkum_pamong/'.$prod);            
+        }
+    }
+
+    function hapuskurikulum()
+    {
+        $nama_kurikulum = $this->input->post('nama_kurikulum');
+        // var_dump($nama_kurikulum);exit();
+        $prod =  $this->input->post('prodi'); 
+
+        $hasil = $this->import_model->del_kurikulum($nama_kurikulum);
+        // var_dump($hasil);exit;
+
+        if (!$hasil) {                          
+            $this->session->set_flashdata('kurkum', 'DATA KURIKULUM GAGAL DIHAPUS.');               
+            redirect('import/kurkum_pamong/'.$prod);            
+        } else {                                
+            $this->session->set_flashdata('kurkum', 'DATA KURIKULUM BERHASIL DIHAPUS.');    
+            redirect('import/kurkum_pamong/'.$prod);            
+        }
+        
+    }
+
+
+     function ubahkurikulumatkul(){
+
+    
+        $ubahmatkul['kode_mk'] = $this->input->post('kode_mk', true);
+        $ubahmatkul['nama_mk'] = $this->input->post('nama_mk', true);
+        $ubahmatkul['jenis_mk'] = $this->input->post('jenis_mk', true);
+        $ubahmatkul['sks_tatapmuka'] = $this->input->post('sks_tatapmuka', true);
+        $ubahmatkul['sks_praktek'] = $this->input->post('sks_praktek', true);
+        $ubahmatkul['sks_lapangan'] = $this->input->post('sks_lapangan', true);
+        $ubahmatkul['sks_simulasi'] = $this->input->post('sks_simulasi', true);
+        $ubahmatkul['tgl_mulai_efektif'] = $this->input->post('tgl_mulai_efektif', true);
+        $ubahmatkul['tgl_akhir_efektif'] = $this->input->post('tgl_akhir_efektif', true);
+        $ubahmatkul['semester'] = $this->input->post('semester', true);
+
+        
+        $prod =  $this->input->post('prodi', true); 
+        $nama_kurikulum =  $this->input->post('nama_kurikulum', true); 
+
+
+        $result = $this->import_model->ubah_matkul($ubahmatkul);
+
+        if (!$result) {                             
+            $this->session->set_flashdata('kurkum_matkul', 'DATA MATA KULIAH GAGAL DIUBAH.');        
+            redirect('import/kurkum_matkul/'.$prod.'/'.$nama_kurikulum);            
+        } else {                                
+            $this->session->set_flashdata('kurkum_matkul', 'DATA  MATA KULIAH BERHASIL DIUBAH.');         
+            redirect('import/kurkum_matkul/'.$prod.'/'.$nama_kurikulum);            
+        }
+    }
+
+    function hapuskurikulummatkul()
+    {
+        $kode_mk = $this->input->post('kode_mk');
+        // var_dump($nama_kurikulum);exit();
+        $prod =  $this->input->post('prodi'); 
+        $nama_kurikulum =  $this->input->post('nama_kurikulum'); 
+
+        $hasil = $this->import_model->del_matkul($kode_mk);
+        // var_dump($hasil);exit;
+
+        if (!$hasil) {                          
+            $this->session->set_flashdata('kurkum_matkul', 'DATA MATA KULIAH GAGAL DIHAPUS.');               
+            redirect('import/kurkum_matkul/'.$prod.'/'.$nama_kurikulum);            
+        } else {                                
+            $this->session->set_flashdata('kurkum_matkul', 'DATA MATA KULIAH BERHASIL DIHAPUS.');    
+            redirect('import/kurkum_matkul/'.$prod.'/'.$nama_kurikulum);            
+        }
+        
+    }
+
 }
--- application/modules/import/models/Import_model.php
+++ application/modules/import/models/Import_model.php
@@ -171,5 +171,67 @@ class Import_model extends CI_Model {
         return $this->db->insert_batch($table, $data);
     }
 
+     //kurikulum
+
+    public function kurkum_matkul($table, $data){
+        $result = $this->db->insert_batch($table, $data);
+
+        if($result){
+            $this->session->set_flashdata('kurkum_matkul', '<div class="alert alert-success"><b>PROSES IMPORT BERHASIL!</b> Data berhasil diimport!</div>');
+            return TRUE;
+        }else{
+            $this->session->set_flashdata('kurkum_matkul', '<div class="alert alert-warning"><b>PROSES IMPORT GAGAL!</b> Data Gagal diimport!</div>');
+            return FALSE;
+        }
+    }
+
+    public function getkurkum($data){
+        return $this->db->query("SELECT * FROM tbl_kurkum where prodi = '$data' ")->result();
+    }
+
+    public function getmatkulkurkum($prodi,$nama_kurikulum){
+        return $this->db->query("SELECT * FROM tbl_kurkum_matkul where prodi = '$prodi' and nama_kurikulum = '$nama_kurikulum' ")->result();
+    }
+
+    function cekdata($nama_kurikulum)
+    {   
+        $sql = $this->db->query("SELECT nama_kurikulum FROM tbl_kurkum where nama_kurikulum='$nama_kurikulum' ")->result();
+        return $sql;
+    }
+
+    function tambahkurikulum($input_data)
+    {   
+        $tamkul = $this->db->insert('tbl_kurkum', $input_data);
+        return $tamkul;
+    }
+
+    function del_kurikulum($nama_kurikulum){
+
+        $hasil=$this->db->query("DELETE FROM tbl_kurkum WHERE nama_kurikulum= '$nama_kurikulum' ");
+        return $hasil;
+    }
+
+    public function ubah_kurikulum($ubahkurkum){
+        
+        $nama_kurikulum = $ubahkurkum['nama_kurikulum'];
+        $ubahkurikulum = $this->db->where('nama_kurikulum', $nama_kurikulum)->update('tbl_kurkum', $ubahkurkum);
+
+        return $ubahkurikulum;  
+    }
+
+     function del_matkul($kode_mk){
+
+        $hasil=$this->db->query("DELETE FROM tbl_kurkum_matkul WHERE kode_mk = '$kode_mk' ");
+        return $hasil;
+    }
+
+    public function ubah_matkul($ubahmatkul){
+        
+        $kode_mk = $ubahmatkul['kode_mk'];
+        $ubahmatkul = $this->db->where('kode_mk', $kode_mk)->update('tbl_kurkum_matkul', $ubahmatkul);
+
+        return $ubahmatkul;  
+    }
+
    
 }
