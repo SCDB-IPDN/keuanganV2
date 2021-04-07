e9ad80a599cac2940a7d6afb4d37c5a0ab3f1180
Maulyanda <maulyanda94.kcb@gmail.com>
2021-04-07 09:59:13 +0700

    plot dosen

--- application/modules/kemeng/models/Kemeng_model.php
+++ application/modules/kemeng/models/Kemeng_model.php
@@ -4,7 +4,7 @@ class Kemeng_model extends CI_Model
 {
 	public function cek_dosen($nip)
 	{
-		$result = $this->db->query("SELECT nip FROM tbl_dosen WHERE nip = $nip")->result();
+		$result = $this->db->query("SELECT nip FROM tbl_dosen WHERE nip = '$nip'")->result();
 
 		return $result;
 	}
@@ -14,7 +14,7 @@ class Kemeng_model extends CI_Model
 		if($nip == 'admin'){
 			$result = $this->db->query("SELECT * FROM tbl_plot_dosen WHERE semester = '$semester'")->result();
 		}else{
-			$result = $this->db->query("SELECT * FROM tbl_plot_dosen WHERE nip = $nip AND semester = '$semester'")->result();
+			$result = $this->db->query("SELECT * FROM tbl_plot_dosen WHERE nip = '$nip' AND semester = '$semester'")->result();
 		}
 
 		return $result;
@@ -27,7 +27,6 @@ class Kemeng_model extends CI_Model
 
 		return $fakul;
 	}
-
 
 	public function get_fakultassss($id_fakultas)
 	{
@@ -43,8 +42,6 @@ class Kemeng_model extends CI_Model
 		return $fakul;
 	}
 
-
-
 	public function get_makul($id_fakultas)
 	{
 		if ($id_fakultas != "Admin"){
@@ -57,12 +54,14 @@ class Kemeng_model extends CI_Model
 		return $makul;
 	}
 
+	public function log($log){
+		return $this->db->insert('tbl_log', $log);
+	}
 
 	function get_sub_category($category_id){
 		$query = $this->db->get_where('tbl_prodi', array('id_fakultas' => $category_id));
 		return $query;
 	}
-
 
 	public function ubah($ubahmatakuliah){
 		
@@ -71,7 +70,6 @@ class Kemeng_model extends CI_Model
 
 		return $ubahmatkul; 	
 	}
-
 
 	function tambahmatkul($input_data)
 	{   
@@ -87,7 +85,7 @@ class Kemeng_model extends CI_Model
 		$this->db->join("tbl_prodi", "tbl_prodi.id_prodi =  tbl_absensi.id_prodi");
 		$this->db->join("tbl_matkul", "tbl_matkul.id_matkul =  tbl_absensi.id_matkul");
 
-		if ($id_absensi != "Admin"){
+		if ($id_absensi != "Admin" || $id_absensi != "SuperAdmin"){
 			$this->db->where("tbl_absensi.tbl_absensi", $id_absensi);
 		} else {
 			$this->db->group_by("tbl_absensi.id_absensi");
@@ -96,13 +94,11 @@ class Kemeng_model extends CI_Model
 		return $this->db->get();
 	}
 
-
 	function cekdata($id)
 	{   
 		$sql = $this->db->query("SELECT id_matkul FROM tbl_matkul where id_matkul='$id' ")->result();
 		return $sql;
 	}
-
 
 	function del_matkul($id_matkul){
 
@@ -161,6 +157,7 @@ class Kemeng_model extends CI_Model
 		
 	}
 
+	//HONOR
 	function get_absen_bulan($date, $nip) {
 		$date_t = explode('-', $date);
 		$index = $this->get_index_honor($nip)->row_array()['index'];
@@ -181,10 +178,9 @@ class Kemeng_model extends CI_Model
 		$res = $this->db->get();
 		return $res;
 	}
+	//END HONOR
 
 	function get_honor_allinone($id_fakultas){
-
-
 		$query = $this->db->query("SELECT tbl_plot_dosen.nip,tbl_plot_dosen.nama,tbl_plot_dosen.nama_matkul,tbl_dosen.jabatan,  sum(tbl_plot_dosen.sks) as totalsks, 9 as kewajiban, 
 			CASE WHEN SUM(tbl_plot_dosen.sks) > 9  THEN SUM(tbl_plot_dosen.sks)-9
 			WHEN SUM(tbl_plot_dosen.sks) <= 9 then 0
@@ -195,57 +191,36 @@ class Kemeng_model extends CI_Model
 			FROM tbl_dosen JOIN tbl_plot_dosen on tbl_dosen.nip= tbl_plot_dosen.nip JOIN tbl_honor_dosen on tbl_honor_dosen.jabatan = tbl_dosen.jabatan
 			WHERE tbl_plot_dosen.id_fakultas ='$id_fakultas' GROUP BY tbl_plot_dosen.nip ");
 		
-
 		return $query;
 	}
 
 	function nihcoba($id_fakultas,$nip){
-		// var_dump($nip);exit();
 		$b = array();
 		$total = count($nip);
-		// var_dump($nip[0]->nip);exit();
+
 		for ($i=0; $i<$total; $i++){
 			$bebas = $nip[$i]->nip;
 
-			// var_dump($bebas);
 			$b []=$bebas;
-			// var_dump($b);
 		}
 
-		// var_dump(json_encode($b));
-		// exit();
-		// foreach ($nip as $a) {
-		// 	$b[] = $a;
-
-		// }
 		$dadah = json_encode($b);
 
 		$bebas= preg_replace('/[^0-9\",]/', '', $dadah);
 		
-		// echo ("SELECT tbl_plot_dosen.nip,tbl_plot_dosen.nama_matkul,tbl_plot_dosen.sks, 9 as kewajiban FROM tbl_dosen JOIN tbl_plot_dosen on tbl_dosen.nip= tbl_plot_dosen.nip JOIN tbl_honor_dosen on tbl_honor_dosen.jabatan = tbl_dosen.jabatan WHERE tbl_plot_dosen.id_fakultas ='$id_fakultas' and tbl_plot_dosen.nip IN ($bebas)");exit();
-
 		if ($bebas != NULL) {
 			$query = $this->db->query("SELECT tbl_plot_dosen.nip,tbl_plot_dosen.nama_matkul,tbl_plot_dosen.sks, 9 as kewajiban FROM tbl_dosen JOIN tbl_plot_dosen on tbl_dosen.nip= tbl_plot_dosen.nip JOIN tbl_honor_dosen on tbl_honor_dosen.jabatan = tbl_dosen.jabatan WHERE tbl_plot_dosen.id_fakultas ='$id_fakultas' and tbl_plot_dosen.nip IN ($bebas)")->result();
 			return $query;
 		}
-
-		
 	}
 
-
 	function cobanip($id_fakultas){
-		// var_dump($nip);exit();
-
 		$query = $this->db->query("SELECT nip FROM tbl_plot_dosen WHERE id_fakultas ='$id_fakultas'");
 		
-
 		return $query;
 	}
 
-
-
 	function get_fakulhonor(){
-		// var_dump($id_fakultas);exit();
 		$query = $this->db->query("SELECT tbl_plot_dosen.nip,tbl_plot_dosen.nama,tbl_dosen.jabatan, sum(tbl_plot_dosen.sks) as totalsks,9 as kewajiban,
 			CASE WHEN SUM(tbl_plot_dosen.sks) > 9  THEN SUM(tbl_plot_dosen.sks)-9
 			WHEN SUM(tbl_plot_dosen.sks) <= 9 then 0
@@ -253,10 +228,8 @@ class Kemeng_model extends CI_Model
 			CASE WHEN SUM(tbl_plot_dosen.sks) > 9  THEN (SUM(tbl_plot_dosen.sks)-9)*(tbl_honor_dosen.index)
 			WHEN SUM(tbl_plot_dosen.sks) <= 9 then 0
 			END AS total
-			FROM `tbl_plot_dosen` join tbl_dosen on tbl_plot_dosen.nip = tbl_dosen.nip JOIN tbl_honor_dosen on tbl_honor_dosen.jabatan = tbl_dosen.jabatan 
-			
+			FROM `tbl_plot_dosen` join tbl_dosen on tbl_plot_dosen.nip = tbl_dosen.nip JOIN tbl_honor_dosen on tbl_honor_dosen.jabatan = tbl_dosen.jabatan
 			group by tbl_plot_dosen.nip");
-			// where tbl_plot_dosen.id_fakultas='$id_fakultas'
 
 		return $query;
 	}
@@ -271,37 +244,111 @@ class Kemeng_model extends CI_Model
 	}
 	
 	function get_all_dosen(){
-
 		$query = "SELECT * from tbl_plot_dosen ";
-		
-		
+
 		return $query;
 	}
-
 
 	function nama_fakultas(){
-
-		$query =  $this->db->query("SELECT id_fakultas from tbl_plot_dosen ");
-		
+		$query =  $this->db->query("SELECT id_fakultas from tbl_plot_dosen ");	
 		
 		return $query;
 	}
 
-
 	function nama_dosen(){
-
 		$query =  $this->db->query("SELECT nip from tbl_plot_dosen ");
-		
-		
+			
 		return $query;
 	}
 
 	function matkul_dosen($nip){
-
 		$query =  $this->db->query("SELECT nip,nama_matkul,sks FROM `tbl_plot_dosen` WHERE nip='$nip'");
-		
-		
+				
 		return $query;
 	}
 
+	//PLOT
+	public function get_all_plot(){
+		$result = $this->db->query("SELECT * FROM tbl_plot_dosen");
+		return $result;
+	}
+
+  	function tambah_plot($plot){
+		$result = $this->db->insert('tbl_plot_dosen', $plot);
+		return $result;
+	}
+	
+	public function get_nama(){
+		$result = $this->db->query("SELECT nama, nip FROM tbl_dosen");
+		return $result;
+	}
+
+	public function get_nama_matkul(){
+		$result = $this->db->query("SELECT nama_matkul, id_matkul FROM tbl_matkul ");
+		return $result;
+	}
+
+	public function get_nama_prodi(){
+		$result = $this->db->query("SELECT nama_prodi, id_prodi FROM tbl_prodi ");
+		return $result;
+	}
+
+	public function get_nama_fakultas(){
+		$result = $this->db->query("SELECT nama_fakultas, id_fakultas FROM tbl_fakultas ");
+		return $result;
+	}
+
+	function edit_plot($editplot){
+		$this->db->where('id_plot', $editplot['id_plot']);
+		$this->db->update('tbl_plot_dosen', $editplot);
+  	}
+
+	function hapus_plot($id){
+
+		$this->db->where(['id_plot' => $id]);
+		$this->db->delete('tbl_plot_dosen');
+	// 	$hasil=$this->db->query("DELETE FROM tbl_plot_dosen WHERE id_plot= '$id' ");
+	// 	return $hasil;
+	}
+	
+	public function ProdiByFakultasId($fakultas_id){
+        $query = $this->db->get_where('tbl_prodi', array('id_fakultas' => $fakultas_id));
+        return $query->result();
+    }
+
+    public function MatkulByProdiId($prodi_id){
+        $query = $this->db->get_where('tbl_matkul', array('id_prodi' => $prodi_id));
+        return $query->result();
+	}
+
+	public function SksByMatkul($sks){
+        $query = $this->db->get_where('tbl_matkul', array('id_matkul' => $sks));
+        return $query->result();
+	}
+
+	public function SemesterByMatkul($semester){
+        $query = $this->db->get_where('tbl_matkul', array('id_matkul' => $semester));
+        return $query->result();
+	}
+	
+	public function getRowFakultas($id){
+		$query = $this->db->get_where('tbl_fakultas', ['id_fakultas' => $id]);
+		return $query->row();
+	}
+	
+	public function getRowProdi($id){
+		$query = $this->db->get_where('tbl_prodi', ['id_prodi' => $id]);
+		return $query->row();
+	}
+	
+	public function getRowMatkul($id){
+		$query = $this->db->get_where('tbl_matkul', ['id_matkul' => $id]);
+		return $query->row();
+	}
+
+	public function getRowSks($id){
+		$query = $this->db->get_where('tbl_matkul', ['id_prodi' => $id]);
+		return $query->row();
+	}
+	//END PLOT
 }
