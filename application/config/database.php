2020b08e8d3a0d9c31db2922bff9f3692fada0ea
Maulyanda <maulyanda94.kcb@gmail.com>
2021-04-13 10:32:22 +0700

    update home dan database again

--- application/config/database.php
+++ application/config/database.php
@@ -98,24 +98,24 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-$db['spcp'] = array(
-	'dsn'	=> '',
-	'hostname' => '180.250.19.85',
-	'username' => 'spcp',
-	'password' => '1pdn-Spcp',
-	'database' => 'spcpipdn21',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+// $db['spcp'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => '180.250.19.85',
+// 	'username' => 'spcp',
+// 	'password' => '1pdn-Spcp',
+// 	'database' => 'spcpipdn21',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
