3b5bfdd7282875381ca05bcbd3437c0c3efe6d14
restiyanada <rfziog@gmail.com>
2021-04-22 13:56:00 +0700

    db

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	// 'hostname' => 'localhost',
+	'hostname' => 'localhost',
 	// 'username' => 'root',
 	// 'password' => '',
-	'hostname' => '192.168.203.153',
+	// 'hostname' => '192.168.203.153',
 	'username' => 'ipdnacid_scdb',
 	'password' => 'yellowminicooper',
 	'database' => 'ipdnacid_scdb',
